a1a9398101a30f1409b6d03f0548e2d2ee787be0
hwolf <heiko>
2025-02-10 12:31:51 +0100

    Initial

---
+++ src/test/java/test/TestApplicationTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
