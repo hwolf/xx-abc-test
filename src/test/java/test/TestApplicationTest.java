985fdfea668f2cf97df5be84104f9dcfd9fc5bd7
hwolf <heiko>
2025-02-15 17:55:41 +0100

    Version

--- src/test/java/test/TestApplicationTest.java
+++ src/test/java/test/TestApplicationTest.java
@@ -3,14 +3,11 @@ package test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Fail.fail;
-
 @SpringBootTest
 class TestApplicationTest {
 
     @Test
     void contextLoads() {
         // Dummy test
-        fail();
     }
 }
